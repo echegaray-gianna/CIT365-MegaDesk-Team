33c945d5680cf23f4d763c633cad3993063d68be
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-02-06 13:39:44 -0700

    Spelling correction

--- MegaDesk-Echegaray/MegaDesk-Echegaray/DisplayQuote.Designer.cs
+++ MegaDesk-Echegaray/MegaDesk-Echegaray/DisplayQuote.Designer.cs
@@ -56,188 +56,170 @@ namespace MegaDesk_Echegaray
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(84, 95);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(112, 117);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(176, 13);
+            this.label3.Size = new System.Drawing.Size(233, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Total Cost according Surface Area: ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(171, 130);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(228, 160);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.Size = new System.Drawing.Size(118, 17);
             this.label4.TabIndex = 3;
             this.label4.Text = "Price per Drawer:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(187, 150);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(249, 185);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.Size = new System.Drawing.Size(89, 17);
             this.label5.TabIndex = 4;
             this.label5.Text = "Drawer Cost:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(211, 186);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(281, 229);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.Size = new System.Drawing.Size(62, 17);
             this.label6.TabIndex = 5;
             this.label6.Text = "Material:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(187, 206);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Location = new System.Drawing.Point(249, 254);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.Size = new System.Drawing.Size(94, 17);
             this.label7.TabIndex = 6;
             this.label7.Text = "Material Cost:";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(171, 240);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Location = new System.Drawing.Point(228, 295);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(93, 13);
+            this.label8.Size = new System.Drawing.Size(122, 17);
             this.label8.TabIndex = 7;
             this.label8.Text = "Shipping  Method:";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(182, 260);
-            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Location = new System.Drawing.Point(243, 320);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(78, 13);
+            this.label9.Size = new System.Drawing.Size(103, 17);
             this.label9.TabIndex = 8;
             this.label9.Text = "Shipping  Cost:";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(197, 317);
-            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Location = new System.Drawing.Point(263, 390);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(58, 13);
+            this.label10.Size = new System.Drawing.Size(76, 17);
             this.label10.TabIndex = 9;
             this.label10.Text = "Total Cost:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(357, -6);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(476, -7);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.Size = new System.Drawing.Size(0, 17);
             this.label2.TabIndex = 10;
             // 
             // areaCost
             // 
             this.areaCost.AutoSize = true;
-            this.areaCost.Location = new System.Drawing.Point(267, 95);
-            this.areaCost.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.areaCost.Location = new System.Drawing.Point(356, 117);
             this.areaCost.Name = "areaCost";
-            this.areaCost.Size = new System.Drawing.Size(243, 13);
+            this.areaCost.Size = new System.Drawing.Size(324, 17);
             this.areaCost.TabIndex = 12;
             this.areaCost.Text = "Here is going to be the total according the surface";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(265, 130);
-            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label13.Location = new System.Drawing.Point(353, 160);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(34, 13);
+            this.label13.Size = new System.Drawing.Size(44, 17);
             this.label13.TabIndex = 13;
             this.label13.Text = "50.00";
             // 
             // materialName
             // 
             this.materialName.AutoSize = true;
-            this.materialName.Location = new System.Drawing.Point(267, 186);
-            this.materialName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.materialName.Location = new System.Drawing.Point(356, 229);
             this.materialName.Name = "materialName";
-            this.materialName.Size = new System.Drawing.Size(140, 13);
+            this.materialName.Size = new System.Drawing.Size(188, 17);
             this.materialName.TabIndex = 14;
             this.materialName.Text = "Here is the material selected";
             // 
             // materialCost
             // 
             this.materialCost.AutoSize = true;
-            this.materialCost.Location = new System.Drawing.Point(267, 206);
-            this.materialCost.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.materialCost.Location = new System.Drawing.Point(356, 254);
             this.materialCost.Name = "materialCost";
-            this.materialCost.Size = new System.Drawing.Size(117, 13);
+            this.materialCost.Size = new System.Drawing.Size(156, 17);
             this.materialCost.TabIndex = 15;
             this.materialCost.Text = "Cost according material";
             // 
             // shippingMethod
             // 
             this.shippingMethod.AutoSize = true;
-            this.shippingMethod.Location = new System.Drawing.Point(269, 240);
-            this.shippingMethod.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.shippingMethod.Location = new System.Drawing.Point(359, 295);
             this.shippingMethod.Name = "shippingMethod";
-            this.shippingMethod.Size = new System.Drawing.Size(21, 13);
+            this.shippingMethod.Size = new System.Drawing.Size(24, 17);
             this.shippingMethod.TabIndex = 16;
             this.shippingMethod.Text = "##";
             // 
             // shippingCost
             // 
             this.shippingCost.AutoSize = true;
-            this.shippingCost.Location = new System.Drawing.Point(269, 260);
-            this.shippingCost.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.shippingCost.Location = new System.Drawing.Point(359, 320);
             this.shippingCost.Name = "shippingCost";
-            this.shippingCost.Size = new System.Drawing.Size(122, 13);
+            this.shippingCost.Size = new System.Drawing.Size(161, 17);
             this.shippingCost.TabIndex = 17;
             this.shippingCost.Text = "Cost according Shipping";
             // 
             // totalCost
             // 
             this.totalCost.AutoSize = true;
-            this.totalCost.Location = new System.Drawing.Point(267, 317);
-            this.totalCost.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.totalCost.Location = new System.Drawing.Point(356, 390);
             this.totalCost.Name = "totalCost";
-            this.totalCost.Size = new System.Drawing.Size(51, 13);
+            this.totalCost.Size = new System.Drawing.Size(67, 17);
             this.totalCost.TabIndex = 18;
             this.totalCost.Text = "total Cost";
             // 
             // drawerCost
             // 
             this.drawerCost.AutoSize = true;
-            this.drawerCost.Location = new System.Drawing.Point(265, 150);
-            this.drawerCost.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.drawerCost.Location = new System.Drawing.Point(353, 185);
             this.drawerCost.Name = "drawerCost";
-            this.drawerCost.Size = new System.Drawing.Size(172, 13);
+            this.drawerCost.Size = new System.Drawing.Size(229, 17);
             this.drawerCost.TabIndex = 19;
             this.drawerCost.Text = "Here is going to be the drawer cost";
             // 
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(298, 240);
-            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label12.Location = new System.Drawing.Point(397, 295);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(31, 13);
+            this.label12.Size = new System.Drawing.Size(40, 17);
             this.label12.TabIndex = 21;
             this.label12.Text = "Days";
             // 
             // backBtn
             // 
-            this.backBtn.Location = new System.Drawing.Point(406, 344);
-            this.backBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.backBtn.Location = new System.Drawing.Point(541, 423);
+            this.backBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.backBtn.Name = "backBtn";
-            this.backBtn.Size = new System.Drawing.Size(75, 25);
+            this.backBtn.Size = new System.Drawing.Size(100, 31);
             this.backBtn.TabIndex = 22;
             this.backBtn.Text = "Back";
             this.backBtn.UseVisualStyleBackColor = true;
@@ -246,38 +228,35 @@ namespace MegaDesk_Echegaray
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(167, 49);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(223, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.Size = new System.Drawing.Size(117, 17);
             this.label1.TabIndex = 23;
-            this.label1.Text = "Custumer Name: ";
+            this.label1.Text = "Customer Name: ";
             // 
             // custName
             // 
             this.custName.AutoSize = true;
-            this.custName.Location = new System.Drawing.Point(267, 49);
-            this.custName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.custName.Location = new System.Drawing.Point(356, 60);
             this.custName.Name = "custName";
-            this.custName.Size = new System.Drawing.Size(82, 13);
+            this.custName.Size = new System.Drawing.Size(109, 17);
             this.custName.TabIndex = 24;
-            this.custName.Text = "Custumer Name";
+            this.custName.Text = "Customer Name";
             // 
             // quoteDate
             // 
             this.quoteDate.AutoSize = true;
-            this.quoteDate.Location = new System.Drawing.Point(221, 19);
-            this.quoteDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.quoteDate.Location = new System.Drawing.Point(295, 23);
             this.quoteDate.Name = "quoteDate";
-            this.quoteDate.Size = new System.Drawing.Size(59, 13);
+            this.quoteDate.Size = new System.Drawing.Size(77, 17);
             this.quoteDate.TabIndex = 25;
             this.quoteDate.Text = "Date Label";
             // 
             // DisplayQuote
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 378);
+            this.ClientSize = new System.Drawing.Size(711, 465);
             this.Controls.Add(this.quoteDate);
             this.Controls.Add(this.custName);
             this.Controls.Add(this.label1);
@@ -300,7 +279,7 @@ namespace MegaDesk_Echegaray
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "DisplayQuote";
             this.Text = "DisplayQuote";
             this.ResumeLayout(false);
