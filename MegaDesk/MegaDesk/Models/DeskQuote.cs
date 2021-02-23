0e7ddd9eaeeccb7f285c036259dcb8474a0806da
echegaray-gianna <ech15002@byui.edu>
2021-02-23 10:21:32 -0700

    project structure

--- MegaDesk/MegaDesk/Models/DeskQuote.cs
+++ MegaDesk/MegaDesk/Models/DeskQuote.cs
@@ -9,14 +9,41 @@ namespace MegaDesk.Models
 {
     public class DeskQuote
     {
-
+        //Quote ID
         public int ID { get; set; }
 
+        //Quote Date
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
 
-        [StringLength(int.MaxValue, MinimumLength = 3)]
+        //CustomerName
+        [Display(Name = "Customer Name"), RegularExpression(@"^[a-zA-Z\s]+$"), StringLength(int.MaxValue, MinimumLength = 3), Required]
         public string CustomerName { get; set; }
 
+        // Desk Width
+
+        [Range(24, 96)]
+        [Column(TypeName = "decimal(4, 2)"), Required]
+        public decimal Width { get; set; }
+
+        // Desk Deep
+        [Range(12, 46)]
+        [Column(TypeName = "decimal(4, 2)"), Required]
+        public decimal Deep { get; set; }
+
+        //Number of Drawers
+        [Range(0, 7), Required]
+        public int Drawers { get; set; }
+
+        //Material
+        [Required]
+        public string Material { get; set; }
+
+        //Shipping 
+        [Required]
+        public int Shipping { get; set; }
 
 
     }
+
 }
