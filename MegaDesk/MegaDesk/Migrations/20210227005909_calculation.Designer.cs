0be8319d19ec27a26b4b4d60089cfc589bd711a3
echegaray-gianna <ech15002@byui.edu>
2021-02-26 18:01:12 -0700

    update

--- MegaDesk/MegaDesk/Migrations/20210227005909_calculation.Designer.cs
+++ MegaDesk/MegaDesk/Migrations/20210227005909_calculation.Designer.cs
@@ -4,14 +4,16 @@ using MegaDesk.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MegaDesk.Migrations
 {
     [DbContext(typeof(MegaDeskContext))]
-    partial class MegaDeskContextModelSnapshot : ModelSnapshot
+    [Migration("20210227005909_calculation")]
+    partial class calculation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MegaDesk/MegaDesk/Migrations/20210227005909_calculation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MegaDesk.Migrations
+{
+    public partial class calculation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Area",
+                table: "DeskQuote",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Cost",
+                table: "DeskQuote",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Area",
+                table: "DeskQuote");
+
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "DeskQuote");
+        }
+    }
+}
