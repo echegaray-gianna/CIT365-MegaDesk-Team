688a5cd8d6a4fe0eb4427eadc19064f2ed456ed9
Kisuk Jeong <kisuk.jeong.lds@gmail.com>
2021-02-27 23:07:47 +0900

    test

---
+++ MegaDesk/MegaDesk/Migrations/20210227140440_details.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MegaDesk.Migrations
+{
+    public partial class details : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
